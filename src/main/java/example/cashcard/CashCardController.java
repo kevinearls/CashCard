52a7dd1d7ead5cca61aab2b8a872faebc2611cbf
Kevin Earls <kevinearls@mac.com>
2023-09-05 18:18:31 +0200

    Added repository stuff
    Signed-off-by: Kevin Earls <kevinearls@mac.com>

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -6,20 +6,37 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/cashcards")
 public class CashCardController {
 
-    @GetMapping("/{requestedId}")
-    public ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
-        System.out.println("requestID: " + requestedId);
-        if (requestedId.equals(99L)) {
-            CashCard cashCard = new CashCard(99L, 123.45);
-            return ResponseEntity.ok(cashCard);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+    private CashCardRepository cashCardRepository;
+
+    public CashCardController(CashCardRepository cashCardRepository) {
+        this.cashCardRepository = cashCardRepository;
     }
+
+//    @GetMapping("/{requestedId}")
+//    public ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
+//        System.out.println("requestID: " + requestedId);
+//        if (requestedId.equals(99L)) {
+//            CashCard cashCard = new CashCard(99L, 123.45);
+//            return ResponseEntity.ok(cashCard);
+//        } else {
+//            return ResponseEntity.notFound().build();
+//        }
+//    }
+@GetMapping("/{requestedId}")
+public ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
+    Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
+    if (cashCardOptional.isPresent()) {
+        return ResponseEntity.ok(cashCardOptional.get());
+    } else {
+        return ResponseEntity.notFound().build();
+    }
+}
 
 
 }
